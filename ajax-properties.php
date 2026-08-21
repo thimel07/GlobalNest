db566c33d5fc0f33440e212e379dce9229c13c3b
Tasnimul Hasan Himel <t.h.himel07@gmail.com>
2026-08-21 14:03:41 +0600

    Update ajax-properties.php

--- ajax-properties.php
+++ ajax-properties.php
@@ -12,111 +12,43 @@ isset($_GET["search"])
 : "";
 
 
-$query =
-"SELECT * FROM properties
+$query = "SELECT * FROM properties WHERE title LIKE '%$search%' OR city LIKE '%$search%'
 
-WHERE
-
-title LIKE '%$search%'
-
-OR city LIKE '%$search%'
-
-OR country LIKE '%$search%'
-
-OR property_type LIKE '%$search%'
-
-ORDER BY id DESC";
+OR country LIKE '%$search%' OR property_type LIKE '%$search%' ORDER BY id DESC";
 
 
-$result =
-mysqli_query($conn, $query);
+$result = mysqli_query($conn, $query);
 
 
 if (mysqli_num_rows($result) == 0) {
-
     echo "<p>No properties found.</p>";
-
     exit();
-
 }
 
-
-while (
-    $property =
-    mysqli_fetch_assoc($result)
-) {
-
-
+while ($property = mysqli_fetch_assoc($result) ) {
     echo "
-
     <div class='country-card'>
-
-        <img
-        src='" .
-        htmlspecialchars(
-            $property["image"]
-        )
-        . "'
-        alt='Property'>
-
-
-        <div>
-
+        <img src='" . htmlspecialchars( $property["image"]). "' alt='Property'>
+    <div>
             <h3>" .
-            htmlspecialchars(
-                $property["title"]
-            )
-            . "</h3>
+            htmlspecialchars( $property["title"] ) . "</h3>
 
-
-            <p>" .
-            htmlspecialchars(
-                $property["city"]
-            )
+            <p>" .  htmlspecialchars(   $property["city"])
             . ", " .
-            htmlspecialchars(
-                $property["country"]
-            )
+            htmlspecialchars(  $property["country"] )
             . "</p>
 
+            <p> $" . $property["price"] . " / month</p>
 
-            <p>
-
-                $" .
-                $property["price"]
+            <p> Safety: " . htmlspecialchars( $property["safety"] )
                 .
-                " / month
-
-            </p>
-
-
-            <p>
-
-                Safety: " .
-                htmlspecialchars(
-                    $property["safety"]
-                )
-                .
-
             "</p>
 
-
-            <a
-            href='property.php?id=" .
-            $property["id"]
-            . "'
-            class='primary-btn'>
-
+            <a href='property.php?id=" .  $property["id"]  . "'  class='primary-btn'>
                 View Property
-
             </a>
-
         </div>
-
     </div>
-
     ";
-
 }
-
 ?>
