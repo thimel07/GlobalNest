278cf69913dc9eda235385a61b57a0c06b9d65e2
Tasnimul Hasan Himel <t.h.himel07@gmail.com>
2026-08-21 14:02:02 +0600

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -5,17 +5,9 @@ $username = "root";
 $password = "";
 $database = "globalnest";
 
-$conn = mysqli_connect(
-    $host,
-    $username,
-    $password,
-    $database
-);
+$conn = mysqli_connect( $host,$username, $password,$database);
 
 if (!$conn) {
-
     die("Database connection failed: " . mysqli_connect_error());
-
 }
-
 ?>
