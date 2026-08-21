c0b8a091d9346c679a4209145c67460a03a7d015
Tasnimul Hasan Himel <t.h.himel07@gmail.com>
2026-08-21 14:07:41 +0600

    Update universities.php

--- universities.php
+++ universities.php
@@ -1,38 +1,20 @@
 <?php
-
 include "includes/db.php";
-
 include "includes/header.php";
-
 
 $country_id =
 isset($_GET["country"])
 ? intval($_GET["country"])
 : 0;
 
+$query = "SELECT universities.*, countries.name AS country_name FROM universities LEFT JOIN
+ countries ON universities.country_id = countries.id WHERE universities.country_id = $country_id";
 
-$query =
-"SELECT universities.*, countries.name AS country_name
-
-FROM universities
-
-LEFT JOIN countries
-ON universities.country_id = countries.id
-
-WHERE universities.country_id = $country_id";
-
-
-$result =
-mysqli_query($conn,$query);
-
+$result = mysqli_query($conn,$query);
 ?>
 
-
 <section class="section">
-
-
     <div class="section-title">
-
         <p>
             UNIVERSITY GUIDE
         </p>
@@ -42,79 +24,36 @@ mysqli_query($conn,$query);
         </h2>
 
         <p>
-            Find universities and nearby
-            accommodation.
+            Find universities and nearby accommodation.
         </p>
-
     </div>
 
-
     <div class="country-grid">
-
-
         <?php
-
         while (
             $university =
             mysqli_fetch_assoc($result)
         ):
-
         ?>
 
-
         <div class="feature-card">
-
-
             <h3>
-
-                <?php
-                echo $university["name"];
-                ?>
-
+                <?php echo $university["name"];?>
             </h3>
 
-
-            <p>
-
+            <p> 
                 City:
-                <?php
-                echo $university["city"];
-                ?>
-
+                <?php echo $university["city"]; ?>
             </p>
-
 
             <p>
-
-                <?php
-                echo $university["description"];
-                ?>
-
-            </p>
-
-
-            <br>
-
-
-            <a
-            href="explore.php?university=<?php echo urlencode($university["name"]); ?>"
-            class="primary-btn">
-
+                <?php echo $university["description"]; ?>
+            </p><br>
+            <a href="explore.php?university=<?php echo urlencode($university["name"]); ?>" class="primary-btn">
                 Find Nearby Homes
-
             </a>
-
-
         </div>
-
-
         <?php endwhile; ?>
-
-
     </div>
-
-
 </section>
-
-
 <?php include "includes/footer.php"; ?>
