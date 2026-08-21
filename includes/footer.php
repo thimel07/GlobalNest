4da1e972d0bca830c3512ef32d162ee4711c8fc9
Tasnimul Hasan Himel <t.h.himel07@gmail.com>
2026-08-21 14:02:17 +0600

    Refactor footer structure and remove extra lines

--- includes/footer.php
+++ includes/footer.php
@@ -1,22 +1,10 @@
 <footer>
-
     <div class="logo">
         Global<span>Nest</span>
     </div>
-
-    <p>
-        Your home, your people, your journey.
-    </p>
-
-    <p>
-        © 2026 GlobalNest
-    </p>
-
+    <p> Your home, your people, your journey. </p>
+    <p>   © 2026 GlobalNest </p>
 </footer>
-
-
 <script src="js/script.js"></script>
-
 </body>
-
 </html>
